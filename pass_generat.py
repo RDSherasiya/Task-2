6e32c7d481c879867d647d315b25d93ae7a96055
RDSherasiya <rushisherasiya@gmail.com>
2023-10-09 13:00:59 +0530

    first commit

---
+++ pass_generat.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+def generate_password(length):
+    #characters to use for generating the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Use random.choice to select characters randomly and join them to form the password
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+def main():
+    try:
+        password_length = int(input("Enter the password length: "))
+
+        # length is valid (greater than 0)
+        if password_length <= 0:
+            print("Password length must be greater than 0.")
+            return
+
+        # Generate the password
+        password = generate_password(password_length)
+
+        # Display the generated password
+        print("Generated Password:", password)
+
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for password length.")
+
+if __name__ == "__main__":
+    main()
